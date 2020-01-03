c71df645429eccc3647810c779a24032b6ad7a1a
Austin <austinkobayashi@gmail.com>
2020-01-02 19:26:16 -0800

    Added basic game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public int NumRounds;
+    public int NumPlayers;
+    public GameObject PlayerPrefab;
+    
+    private List<GameObject> _players = new List<GameObject>();
+    private Dictionary<string, int> _scores = new Dictionary<string, int>();
+    private int _curRound = 1;
+    
+    
+    void Awake() {
+        SpawnPlayers();
+    }
+
+
+    void Update() {
+    }
+
+
+    //TODO: base spawn location on number of players
+    void SpawnPlayers() {
+        bool populateDict = _scores.Count == 0;
+
+        for (int i = 0; i < NumPlayers; i++) {
+            Vector3 spawnLocation;
+            if (i == 0) {
+                spawnLocation = new Vector3(-5, Random.Range(-3f, 3f), 0);
+            } else {
+                spawnLocation = new Vector3(5, Random.Range(-3f, 3f), 0);
+            }
+
+            GameObject player = Instantiate(PlayerPrefab, spawnLocation, Quaternion.identity);
+            player.GetComponent<Player>().SetGameManager(this);
+            player.name = "Player" + i;
+            
+            if (populateDict)
+                _scores.Add(player.name, 0);
+            
+            if (i == 0) {
+                player.GetComponent<SpriteRenderer>().color = Color.red;
+                player.GetComponent<Player>().Player1 = true;
+            } else {
+                player.GetComponent<SpriteRenderer>().color = Color.blue;
+            }
+            
+            _players.Add(player);
+        }
+    }
+
+
+    
+    void Reset() {
+        foreach (GameObject player in _players) {
+            Destroy(player);
+        }
+        _players.Clear();
+        SpawnPlayers();
+    }
+
+
+
+    void EndRound() {
+        if (_players.Count < 1)
+            Debug.Log("No winner!");
+        if (_players.Count == 1) {
+            Debug.Log(_players[0].name + " won the round!");
+            _scores[_players[0].name]++;
+        }
+
+        _curRound++;
+        if (_curRound >= NumRounds)
+            EndGame();
+        else
+            Reset();
+    }
+
+
+    void EndGame() {
+        int bestScore = 0;
+        string winner = "";
+        
+        foreach(KeyValuePair<string, int> entry in _scores) {
+            if (entry.Value > bestScore) {
+                bestScore = entry.Value;
+                winner = entry.Key;
+            }
+        }
+        
+        Debug.Log(winner + " wins!");
+    }
+    
+    
+    
+    public void Die(GameObject gameObject) {
+        _players.Remove(gameObject);
+        if (_players.Count <= 1) {
+            EndRound();
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEditorInternal;
 using UnityEngine;
@@ -6,23 +7,25 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
 	public Transform Arrow;
-	
+
 	private SpriteRenderer _arrowSpriteRenderer;
 	private Rigidbody2D _rigid;
 
 	public float Speed;
-	
+
 	public float RotateFrequency;
 	public float RotationAmount;
 	public float PowerStageTimer;
 	public int MaxPower;
 	public float DamageModifier;
-	
+
 	public int IFrameAmount;
-	
+
 	public bool Player1;
 
 	public float Health;
+
+	public bool DebugStatements;
 	
 	private float _rotateTimer = 0;
 	private float _angle = 0;
@@ -30,21 +33,23 @@ public class Player : MonoBehaviour {
 	private float _powerTimer;
 	private bool _attacking;
 	private int _iFramesCount;
-	
-	
-	
-	void Awake () {
+
+	private GameManager _gameManager;
+
+	void Awake() {
 		_arrowSpriteRenderer = Arrow.GetComponent<SpriteRenderer>();
 		_rigid = GetComponent<Rigidbody2D>();
 	}
-	
-	
 
-	void Update () {
-		if (_iFramesCount > 0) { _iFramesCount--; }
-		
+
+
+	void Update() {
+		if (_iFramesCount > 0) {
+			_iFramesCount--;
+		}
+
 		_rotateTimer += Time.deltaTime;
-		
+
 		if (_rotateTimer >= RotateFrequency) {
 			Rotate();
 			_rotateTimer = 0;
@@ -52,65 +57,64 @@ public class Player : MonoBehaviour {
 	}
 
 
-	
-    void FixedUpdate() {
-	    if (_rigid.velocity == Vector2.zero)
-		    _attacking = false;
-	    
-	    
-	    if (Player1) {
-		    if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Z)) {
-			    _power = 0;
-		    }
 
-		    if (Input.GetMouseButton(0) || Input.GetKey(KeyCode.Z)) {
-			    _powerTimer += Time.deltaTime;
-		    }
+	void FixedUpdate() {
+		if (_rigid.velocity == Vector2.zero)
+			_attacking = false;
 
-		    if (_powerTimer >= PowerStageTimer) {
-			    _power++;
-			    _powerTimer = 0;
-		    }
-		    _power = Mathf.Min(_power, MaxPower);
 
-		    if (Input.GetMouseButtonUp(0) || Input.GetKeyUp(KeyCode.Z)) {
-			    Shoot();
-		    }
-	    }
-	    else {
-		    if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.M)) {
-			    _power = 0;
-		    }
+		if (Player1) {
+			if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Z)) {
+				_power = 0;
+			}
 
-		    if (Input.GetMouseButton(0) || Input.GetKey(KeyCode.M)) {
-			    _powerTimer += Time.deltaTime;
-		    }
+			if (Input.GetMouseButton(0) || Input.GetKey(KeyCode.Z)) {
+				_powerTimer += Time.deltaTime;
+			}
 
-		    if (_powerTimer >= PowerStageTimer) {
-			    _power++;
-			    _powerTimer = 0;
-		    }
-		    _power = Mathf.Min(_power, MaxPower);
-		    
-		    if (Input.GetMouseButtonUp(0) || Input.GetKeyUp(KeyCode.M)) {
-			    Shoot();
-		    }
-	    }
-	    
-	    _arrowSpriteRenderer.color = new Vector4((float)_power / (float)MaxPower, 0f, 0f, 1f);
-    }
+			if (_powerTimer >= PowerStageTimer) {
+				_power++;
+				_powerTimer = 0;
+			}
 
-	
+			_power = Mathf.Min(_power, MaxPower);
+
+			if (Input.GetMouseButtonUp(0) || Input.GetKeyUp(KeyCode.Z)) {
+				Shoot();
+			}
+		}
+		else {
+			if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.M)) {
+				_power = 0;
+			}
+
+			if (Input.GetMouseButton(0) || Input.GetKey(KeyCode.M)) {
+				_powerTimer += Time.deltaTime;
+			}
+
+			if (_powerTimer >= PowerStageTimer) {
+				_power++;
+				_powerTimer = 0;
+			}
+
+			_power = Mathf.Min(_power, MaxPower);
+
+			if (Input.GetMouseButtonUp(0) || Input.GetKeyUp(KeyCode.M)) {
+				Shoot();
+			}
+		}
+
+		_arrowSpriteRenderer.color = new Vector4((float) _power / (float) MaxPower, 0f, 0f, 1f);
+	}
+
+
 
 	void Rotate() {
-/*
-		Arrow.RotateAround(transform.position, Vector3.forward, RotationAmount);
-*/
 		_angle += RotationAmount;
 		if (_angle >= 360) {
 			_angle = 0;
 		}
-		
+
 		RotateToAngle(Arrow, transform.position, _angle);
 	}
 
@@ -120,57 +124,68 @@ public class Player : MonoBehaviour {
 		Vector3 angleVec = (transform.position - origin).normalized;
 		Vector3 originVec = Vector3.right;
 		float curRotation = Mathf.Acos(Vector3.Dot(angleVec, originVec)) * Mathf.Rad2Deg;
-		if (angleVec.y < 0) { curRotation = 360 - curRotation; }
+		if (angleVec.y < 0) {
+			curRotation = 360 - curRotation;
+		}
 
 		float angleDelta = angle - curRotation;
 		transform.RotateAround(origin, Vector3.forward, angleDelta);
 	}
-	
-	
-	
+
+
+
 
 	void Shoot() {
 		_attacking = true;
 		_powerTimer = 0;
-		_rigid.AddForce(Speed * _power * (new Vector2(Mathf.Cos(Mathf.Deg2Rad * _angle), Mathf.Sin(Mathf.Deg2Rad * _angle)).normalized), ForceMode2D.Impulse);
+		_rigid.AddForce(
+			Speed * _power * (new Vector2(Mathf.Cos(Mathf.Deg2Rad * _angle), Mathf.Sin(Mathf.Deg2Rad * _angle))
+				.normalized), ForceMode2D.Impulse);
 		_power = 0;
 	}
 
 
-	
+
 	// TODO: slow collisions will deal 0 damage. Do we want it to do at least 1?
 	void OnCollisionEnter2D(Collision2D other) {
 		if (!_attacking)
 			return;
-		
+
 		if (other.gameObject.CompareTag("Player")) {
 			if (other.gameObject.GetComponent<Player>().Invincible()) {
-				Debug.Log(other.gameObject.name  + " is invincible to hit from " + gameObject.name);
+				if (DebugStatements)
+					Debug.Log(other.gameObject.name + " is invincible to hit from " + gameObject.name);
 				return;
 			}
 
-			Debug.Log(other.relativeVelocity.magnitude);
 			int damage = Mathf.RoundToInt(other.relativeVelocity.magnitude * DamageModifier / Speed);
 			other.gameObject.GetComponent<Player>().TakeDamage(damage);
-			Debug.Log(gameObject.name + " damaged " + other.gameObject.name + " for " + damage);
+			if (DebugStatements)
+				Debug.Log(gameObject.name + " damaged " + other.gameObject.name + " for " + damage);
 		}
 	}
 
-	
-	
+
+
 	public void TakeDamage(int amount) {
 		Health -= amount;
-		
+
 		_iFramesCount = IFrameAmount;
-		
+
 		if (Health <= 0) {
+			_gameManager.Die(gameObject);
 			Destroy(gameObject);
 		}
 	}
-	
+
 
 
 	public bool Invincible() {
 		return _iFramesCount > 0;
+	}
+
+
+	public void SetGameManager(GameManager gameManager) {
+		_gameManager = gameManager;
 	}
 }
