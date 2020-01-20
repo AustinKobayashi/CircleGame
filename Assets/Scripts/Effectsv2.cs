defb1604362174f85539323958713b8aa89dd76f
Giulio Rossi <ciuffi9@mac.com>
2020-01-20 15:50:38 -0800

    Refactor effects so its less cancer

--- Assets/Scripts/Effectsv2.cs
+++ Assets/Scripts/Effectsv2.cs
@@ -3,28 +3,26 @@ using System.Threading.Tasks;
 
 public class Effectsv2 : MonoBehaviour {
     public float SloMoDuration;
-    public float ZoomSpeed;
     public float SloMoAmount;
+    public float ZoomSpeed;
+    public float ZoomAmount;
     public float ScreenShakeDuration;
     public float DecreaseFactor = 1.0f;
     public float MaxShakeForce = 40;
-
-    private Vector3 _originalPos;
-    private Transform _camTransform;
-    private float _shakeDuration = 0f;
-    private float _shakeForce;
     [HideInInspector]
     public bool SlowDown;
+    private Transform _camTransform;
+    private Vector3 _origCamPos;
     private float _origFixedTime;
+    private float _origCameraZoom;
+    private float _shakeDuration = 0f;
+    private float _shakeForce;
 
-
-    public int getMaxSlowMo(){
-        return (int)(SloMoDuration * 1000);
-    }
     void Awake() {
         _camTransform = Camera.main.transform;
-        _originalPos = _camTransform.localPosition;
+        _origCamPos = _camTransform.position;
         _origFixedTime = Time.fixedDeltaTime;
+        _origCameraZoom = Camera.main.orthographicSize;
     }
     private void Update() {
         Time.fixedDeltaTime = this._origFixedTime * Time.timeScale;
@@ -44,33 +42,43 @@ public class Effectsv2 : MonoBehaviour {
         _shakeDuration = 0f;
         ResetCamera();
     }
-
+    public void cameraZoom(GameObject pos1, GameObject pos2){
+        Time.timeScale = SloMoAmount;
+         if (!SlowDown){
+            ZoomIn(pos1, pos2);
+        }
+        SlowMoCountDown();
+    }
+    public async void ResetCamera() {
+        Time.timeScale = 1;
+        if (SlowDown) await ZoomOut();
+        _camTransform.position = _origCamPos;
+    }
     async void ZoomIn(GameObject player1, GameObject player2){
-        Debug.Log("Zooming in");
         SlowDown = true;
         var startTime = Time.time;
         var counter = 0;
         var origCameraSize = Camera.main.orthographicSize;
-        var origCameraLocation = Camera.main.transform.position;
-        while (SlowDown && Camera.main.orthographicSize > 3){
+        var origCameraLocation = _camTransform.position;
+        while (SlowDown && Camera.main.orthographicSize > ZoomAmount){
             float t = ((startTime + counter) - startTime) * (ZoomSpeed * SloMoAmount);
-            Camera.main.orthographicSize = Mathf.SmoothStep(origCameraSize, 3, t);
+            Camera.main.orthographicSize = Mathf.SmoothStep(origCameraSize, ZoomAmount, t);
             Vector3 camTarget = GetCameraVector(player1, player2);
-            Camera.main.transform.position = Vector3.Lerp(origCameraLocation, camTarget, t);
+            _camTransform.position = Vector3.Lerp(origCameraLocation, camTarget, t);
             counter++;
             await Task.Delay(1);
         }
     }
-    async void ZoomOut(){
+    async Task ZoomOut(){
         SlowDown = false;
         var startTime = Time.time;
         var counter = 0;
         var origCameraSize = Camera.main.orthographicSize;
-        var origCameraLocation = Camera.main.transform.position;
-        while (!SlowDown && Camera.main.orthographicSize < 5){
+        var origCameraLocation = _camTransform.position;
+        while (!SlowDown && Camera.main.orthographicSize < _origCameraZoom){
             float t = ((startTime + counter) - startTime) * (ZoomSpeed * SloMoAmount);
-            Camera.main.orthographicSize = Mathf.SmoothStep(origCameraSize, 5, t);
-            Camera.main.transform.position = Vector3.Lerp(origCameraLocation, _originalPos, t);
+            Camera.main.orthographicSize = Mathf.SmoothStep(origCameraSize, _origCameraZoom, t);
+            _camTransform.position = Vector3.Lerp(origCameraLocation, _origCamPos, t);
             counter++;
             await Task.Delay(1);
         }
@@ -90,20 +98,12 @@ public class Effectsv2 : MonoBehaviour {
         }
         return new Vector3(normalized.x, normalized.y, -10);
     }
+    public int getMaxSlowMo(){
+        return (int)(SloMoDuration * 1000);
+    }
     private async void SlowMoCountDown(){
 		await Task.Delay(getMaxSlowMo());
         if (SlowDown)
             ResetCamera();
 	}
-    public void cameraZoom(GameObject pos1, GameObject pos2){
-        Time.timeScale = SloMoAmount;
-         if (!SlowDown){
-            ZoomIn(pos1, pos2);
-        }
-        SlowMoCountDown();
-    }
-    public void ResetCamera() {
-        Time.timeScale = 1;
-        if (SlowDown) ZoomOut();
-    }
 }
