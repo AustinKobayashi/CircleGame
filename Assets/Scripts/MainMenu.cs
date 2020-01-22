8200a75d2aa8aa9671076d489468ea272b4861a1
Giulio Rossi <ciuffi9@mac.com>
2020-01-21 22:41:40 -0800

    Add local/online mode to menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,19 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
+    public Button LocalPlayerButton;
+    
+    public Button MultiplayerButton;
+    public string LocalScene;
+    public string MultiplayerScene;
     // Start is called before the first frame update
     void Start()
     {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space) || Input.touchCount > 0){
-            UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("Game");
-        }
+		LocalPlayerButton.onClick.AddListener(() => {
+            UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(LocalScene);
+        });
+        MultiplayerButton.onClick.AddListener(() => {
+            UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(MultiplayerScene);
+        });
     }
 }
---
+++ Assets/Scripts/MultiplayerManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiplayerManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.anyKey || Input.touchCount > 0)
+            UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("MainMenu");
+        
+    }
+}
